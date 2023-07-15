0f22a252aecb5556e1fec1166cf01c0c04069ba1
STC1702 <stc17022002@gmail.com>
2023-07-16 00:31:19 +0700

    Combine Column and Row

--- quizz_app/lib/results_screen.dart
+++ quizz_app/lib/results_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:quizz_app/data/questions.dart';
+import 'package:quizz_app/question_summary.dart';
+
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
     super.key,
@@ -8,26 +11,43 @@ class ResultsScreen extends StatelessWidget {
 
   final List<String> chosenAnswers;
 
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: Container(
-        margin: const EdgeInsets.all(30),
+        margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Text('Your answered X out of Y questions correctly!'),
-            const SizedBox(height: 30),
-            const Text('List of Answers/Questions: ...'),
-            const SizedBox(height: 30),
-            TextButton.icon(
+            const Text('You answered X out of Y questions correctly!'),
+            const SizedBox(
+              height: 30,
+            ),
+            QuestionsSummary(getSummaryData()),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton(
               onPressed: () {},
-              icon: const Icon(
-                Icons.restart_alt,
-              ),
-              label: const Text('Restart Quiz'),
+              child: const Text('Restart Quiz!'),
             )
           ],
         ),
