0b04922974c54f2f7d34e2aaa5036feabe0182e0
STC1702 <stc17022002@gmail.com>
2023-07-15 22:14:52 +0700

    Answer Buttons

--- quizz_app/lib/questions_screen.dart
+++ quizz_app/lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,6 +12,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreen extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Questions Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'The Question...',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: 'Answer', onTap: () {}),
+          AnswerButton(answerText: 'Answer', onTap: () {}),
+          AnswerButton(answerText: 'Answer', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
