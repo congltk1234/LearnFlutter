d5381c806dd4710b1bc7b722afb3a9b4d885c501
STC1702 <stc17022002@gmail.com>
2023-07-16 17:23:39 +0700

    Validating User Input & Show Error dialog

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/data/data.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/add_expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
@@ -16,8 +17,14 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpensesOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      registeredExpenses.add(expense);
+    });
   }
 
   @override
