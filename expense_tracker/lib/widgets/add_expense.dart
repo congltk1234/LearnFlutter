8e99e6390adb3747623cb8418e65a791074f1efd
STC1702 <stc17022002@gmail.com>
2023-07-16 16:02:01 +0700

    Form Controller

--- expense_tracker/lib/widgets/add_expense.dart
+++ expense_tracker/lib/widgets/add_expense.dart
@@ -10,18 +10,49 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
   @override
-  Widget build(BuildContext context) => const Padding(
-        padding: EdgeInsets.all(16),
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
           children: [
             TextField(
+              controller: _titleController,
               maxLength: 40,
               keyboardType: TextInputType.text,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 label: Text('Title'),
               ),
             ),
+            TextField(
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                prefixText: '\$',
+                label: Text('Amount'),
+              ),
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: Text('Add Expense'),
+                ),
+                TextButton(onPressed: () {}, child: Text('Cancel')),
+              ],
+            )
           ],
         ),
       );
