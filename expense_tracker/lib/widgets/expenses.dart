5ba41ec4fe345261e7b02dd32a56d39f3fe24a1f
STC1702 <stc17022002@gmail.com>
2023-07-16 15:35:08 +0700

    Add App Bar & change theme

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -17,6 +17,15 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
       body: Column(
         children: [
           Expanded(child: ExpensesList(expenses: registeredExpenses)),
